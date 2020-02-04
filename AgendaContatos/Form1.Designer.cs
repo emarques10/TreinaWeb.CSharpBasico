1dfd042412df9447ffbef1be0237aa35881405b2
Ewerton Marques <marquescpv@gmail.com>
2020-02-04 12:35:05 -0300

    Projeto Agenda Finalizado

---
+++ AgendaContatos/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace AgendaContatos
+{
+    partial class frmAgendaContatos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbNome = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.txbNumeroTelefone = new System.Windows.Forms.TextBox();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lxbContatos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(315, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            // 
+            // txbNome
+            // 
+            this.txbNome.Location = new System.Drawing.Point(374, 20);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(215, 20);
+            this.txbNome.TabIndex = 2;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(315, 54);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 3;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.Location = new System.Drawing.Point(374, 46);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(215, 20);
+            this.txbEmail.TabIndex = 4;
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(315, 84);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(52, 13);
+            this.lblTelefone.TabIndex = 5;
+            this.lblTelefone.Text = "Telefone:";
+            // 
+            // txbNumeroTelefone
+            // 
+            this.txbNumeroTelefone.Location = new System.Drawing.Point(374, 76);
+            this.txbNumeroTelefone.Name = "txbNumeroTelefone";
+            this.txbNumeroTelefone.Size = new System.Drawing.Size(215, 20);
+            this.txbNumeroTelefone.TabIndex = 6;
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(318, 125);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(75, 23);
+            this.btnIncluir.TabIndex = 8;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.btnIncluir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(318, 155);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 11;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(399, 125);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 13;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(481, 125);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 14;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(400, 154);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 15;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // lxbContatos
+            // 
+            this.lxbContatos.FormattingEnabled = true;
+            this.lxbContatos.Location = new System.Drawing.Point(13, 13);
+            this.lxbContatos.Name = "lxbContatos";
+            this.lxbContatos.Size = new System.Drawing.Size(296, 277);
+            this.lxbContatos.TabIndex = 16;
+            this.lxbContatos.SelectedIndexChanged += new System.EventHandler(this.lxbContatos_SelectedIndexChanged);
+            // 
+            // frmAgendaContatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 305);
+            this.Controls.Add(this.lxbContatos);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.txbNumeroTelefone);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.txbEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txbNome);
+            this.Controls.Add(this.label1);
+            this.Name = "frmAgendaContatos";
+            this.Text = "Agenda de Contatos";
+            this.Shown += new System.EventHandler(this.frmAgendaContatos_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbNome;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.TextBox txbNumeroTelefone;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.ListBox lxbContatos;
+    }
+}
+
---
+++ AgendaContatos/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgendaContatos
+{
+    public partial class frmAgendaContatos : Form
+    {
+        private OperacaoEnum acao;
+        public frmAgendaContatos()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAgendaContatos_Shown(object sender, EventArgs e)
+        {
+            AlterarBotoesSalvarECancelar(false);
+            AlterarBotoesIncluirAlterarExcluir(true);
+            CarregarListaContatos();
+            AlterarEstadoCampos(false);
+        }
+
+        private void AlterarBotoesSalvarECancelar(bool estado)
+        {
+            btnSalvar.Enabled = estado;
+            btnCancelar.Enabled = estado;
+        }
+
+        private void AlterarBotoesIncluirAlterarExcluir(bool estado)
+        {
+            btnIncluir.Enabled = estado;
+            btnAlterar.Enabled = estado;
+            btnExcluir.Enabled = estado;
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            AlterarBotoesSalvarECancelar(true);
+            AlterarBotoesIncluirAlterarExcluir(false);
+            AlterarEstadoCampos(true);
+            acao = OperacaoEnum.INCLUIR;
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            AlterarBotoesSalvarECancelar(true);
+            AlterarBotoesIncluirAlterarExcluir(false);
+            AlterarEstadoCampos(true);
+            acao = OperacaoEnum.ALTERAR;
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Tem certeza?", "Pergunta", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                int indiceExcluido = lxbContatos.SelectedIndex;
+                lxbContatos.SelectedIndex = 0;
+                lxbContatos.Items.RemoveAt(indiceExcluido);
+                List<Contato> contatosList = new List<Contato>();
+                foreach(Contato contato in lxbContatos.Items)
+                {
+                    contatosList.Add(contato);
+                }
+                ManipuladorArquivos.EscreverArquivo(contatosList);
+                CarregarListaContatos();
+                LimparCampos();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            AlterarBotoesSalvarECancelar(false);
+            AlterarBotoesIncluirAlterarExcluir(true);
+            AlterarEstadoCampos(false);
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Contato contato = new Contato
+            {
+                Nome = txbNome.Text,
+                Email = txbEmail.Text,
+                NumeroTelefone = txbNumeroTelefone.Text
+            };
+            List<Contato> contatosList = new List<Contato>();
+            foreach (Contato contatoDaLista in lxbContatos.Items)
+            {
+                contatosList.Add(contatoDaLista);
+            }
+            if(acao == OperacaoEnum.INCLUIR)
+            {
+                contatosList.Add(contato);
+            }
+            else
+            {
+                int indice = lxbContatos.SelectedIndex;
+                contatosList.RemoveAt(indice);
+                contatosList.Insert(indice, contato);
+            }
+
+            ManipuladorArquivos.EscreverArquivo(contatosList);
+            CarregarListaContatos();
+            AlterarBotoesSalvarECancelar(false);
+            AlterarBotoesIncluirAlterarExcluir(true);
+            LimparCampos();
+            AlterarEstadoCampos(false);
+        }
+
+        private void CarregarListaContatos()
+        {
+            lxbContatos.Items.Clear();
+            lxbContatos.Items.AddRange(ManipuladorArquivos.LerArquivo().ToArray());
+        }
+
+        private void LimparCampos()
+        {
+            txbNome.Text = "";
+            txbEmail.Text = "";
+            txbNumeroTelefone.Text = "";
+        }
+
+        private void AlterarEstadoCampos(bool estado)
+        {
+            txbNome.Enabled = estado;
+            txbEmail.Enabled = estado;
+            txbNumeroTelefone.Enabled = estado;
+        }
+
+        private void lxbContatos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Contato contato = (Contato)lxbContatos.Items[lxbContatos.SelectedIndex];
+            txbNome.Text = contato.Nome;
+            txbEmail.Text = contato.Email;
+            txbNumeroTelefone.Text = contato.NumeroTelefone;
+        }
+    }
+}
